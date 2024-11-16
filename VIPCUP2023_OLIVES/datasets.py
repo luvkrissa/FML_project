ed825456edc7a374a26fedfe5fa48fc7578d2431
Tianwei Jiang <tjiang78@gatech.edu>
2024-11-16 02:04:22 -0500

    rename train test set

--- VIPCUP2023_OLIVES/datasets.py
+++ VIPCUP2023_OLIVES/datasets.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import torch
 
+
 class OLIVES(data.Dataset):
     def __init__(self,df, img_dir, transforms):
         self.img_dir = img_dir
@@ -19,13 +20,23 @@ class OLIVES(data.Dataset):
         image = np.array(image)
         image = Image.fromarray(image)
         image = self.transforms(image)
-        b1 = self.df.iloc[idx,1]
-        b2 = self.df.iloc[idx,2]
-        b3 = self.df.iloc[idx,3]
-        b4 = self.df.iloc[idx, 4]
-        b5 = self.df.iloc[idx, 5]
-        b6 = self.df.iloc[idx, 6]
-        bio_tensor = torch.tensor([b1, b2, b3, b4, b5, b6])
+        b1 = self.df.iloc[idx,2]
+        b2 = self.df.iloc[idx,3]
+        b3 = self.df.iloc[idx,4]
+        b4 = self.df.iloc[idx, 5]
+        b5 = self.df.iloc[idx, 6]
+        b6 = self.df.iloc[idx, 7]
+        b7 = self.df.iloc[idx, 8]
+        b8 = self.df.iloc[idx, 9]
+        b9 = self.df.iloc[idx, 10]
+        b10 = self.df.iloc[idx, 11]
+        b11 = self.df.iloc[idx, 12]
+        b12 = self.df.iloc[idx, 13]
+        b13 = self.df.iloc[idx, 14]
+        b14 = self.df.iloc[idx, 15]
+        b15 = self.df.iloc[idx, 16]
+        b16 = self.df.iloc[idx, 17]
+        bio_tensor = torch.tensor([b1, b2, b3, b4, b5, b6,blabla])
         return image, bio_tensor
 
 
